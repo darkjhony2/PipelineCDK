620013a8b632c588553d3b956fc3e169ced57645
Johnny Quezada <jhonnyquezada1997@gmail.com>
2022-10-13 12:44:32 -0500

    adding stages of lambda stack

--- lib/pipeline_cdk-stack.ts
+++ lib/pipeline_cdk-stack.ts
@@ -1,6 +1,7 @@
+import { MyPipeLineAppStage } from './my-pipeline-app-stage';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines'
+import { CodePipeline, CodePipelineSource, ManualApprovalStep, ShellStep } from 'aws-cdk-lib/pipelines'
 
 export class PipelineCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,5 +14,27 @@ export class PipelineCdkStack extends cdk.Stack {
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+
+    const testingStage = pipeline.addStage(new MyPipeLineAppStage(this, "test", {
+      env: {account: "935948034248", region: 'us-east-1'}
+    }));
+
+    testingStage.addPost(new ManualApprovalStep('approval'));
+
+    testingStage.addPost(new ShellStep("validate", {
+      commands: ['curl -Ssf https://my.webservice.com/'],
+    }));
+
+    const wave = pipeline.addWave('wave');
+
+    wave.addStage(new MyPipeLineAppStage(this, 'MyAppUS2', {
+      env: {account: "935948034248", region: 'us-east-2'}
+    }));
+
+    wave.addStage(new MyPipeLineAppStage(this, 'MyAppEU1', {
+      env: {account: "935948034248", region: "eu-west-1"}
+    }));
+
+    
   }
 }
